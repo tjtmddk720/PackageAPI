2843b66beb2ef38717b392504a6b658695d037a6
Seo SeungA <83910139+tjtmddk720@users.noreply.github.com>
2024-06-27 10:00:43 +0900

    refactor : Image entity

--- package/src/main/java/com/example/demo/packages/entity/Image.java
+++ package/src/main/java/com/example/demo/packages/entity/Image.java
@@ -13,18 +13,23 @@ public class Image {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    
     private String filename;
+    
     @Enumerated(value = EnumType.STRING)
     private Type type;
+    
     @ManyToOne
     @JoinColumn(name = "package_id")
     private Package aPackage;
+    
     public enum Type{
         PKG("Package"),
         LBL("Label"),
         DOC("Document"),
         SIGN("Signature"),
         DAM("Damage");
+        
         @Getter
         private String type;
 
